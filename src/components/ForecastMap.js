bffe70edc9ad070ce3ca9a0db684b110a5476615
decka <steriuandreea@yahoo.ro>
2020-03-15 16:07:34 +0100

    Update ForecastMap.js

--- src/components/ForecastMap.js
+++ src/components/ForecastMap.js
@@ -2,14 +2,50 @@ import React, { Component } from "react";
 
 export default class ForecastMap extends Component {
   render() {
-    console.log(this.props);
     const { items } = this.props;
+    const finalObj = {};
+    items.forEach(item => {
+      const time = new Date(item.dt_txt);
+      const day = time.toLocaleDateString("en-us", { weekday: "long" });
+
+      if (finalObj[day]) {
+        finalObj[day].push(item);
+      } else {
+        finalObj[day] = [item];
+      }
+    });
+    console.log(finalObj);
+    // console.log(final);
+    console.log(this.props);
+    const weekday =
+      finalObj[
+        ("Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday")
+      ];
+    console.log(weekday);
+    // const Tuesday = finalObj["Tuesday"];
+    // const Wednesday = finalObj["Wednesday"];
+    // const Thursday = finalObj["Thursday"];
+    // const Friday = finalObj["Friday"];
+    // const Saturday = finalObj["Saturday"];
+    // const Sunday = finalObj["Sunday"];
 
     return (
       <div>
-        {items.map(item => (
-          <div key={item.dt}>{item.dt_txt}</div>
-        ))}
+        <div>
+          {day}
+          {weekday.map(day => (
+            <div key={day.dt}>
+              {day.time}
+              {day.main.temp}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
